8c15ec8decdd23190b6f5dbda80a220b7e89e9f7
DeepRathore <rdeepika342@gmail.com>
2020-06-20 15:27:54 +0530

    update

--- JqueryAssignmentForm/assets/main.js
+++ JqueryAssignmentForm/assets/main.js
@@ -21,70 +21,57 @@ $(document).ready(function() {
             $('#height').val(ui.value + "cm")
         }
     });
-
     $("#height").val($("#slider-height").slider("value") + "cm");
 
     //for validations
     //email
     $('#email').keyup(function() {
         if (validateEmail()) {
-            //if validated
-            $('#email').css("border", "2px solid green");
-            $('#message').html("**Email Validated");
-            $('#message').css("color", "green");
+            $(this).css("border", "2px solid #008000"),
+            $('#message').html("**Email Validated").css("color", "#008000");
         } else {
-            $('#email').css("border", "2px solid red");
-            $('#message').html("**Enter A valid Email address");
-            $('#message').css("color", "red");
+            $(this).css("border", "2px solid #FF0000"),
+            $('#message').html("**Enter A valid Email address").css("color", "#FF0000");
         }
     });
 
     //name
     $('#name').keyup(function() {
         if (validateName()) {
-            //if validated
-            $('#name').css("border", "2px solid green");
-            $('#nameMessage').html("**Name Validated");
-            $('#nameMessage').css("color", "green");
+            $(this).css("border", "2px solid #008000"),
+            $('#nameMessage').html("**Name Validated").css("color", "#008000");
 
         } else {
-            $('#name').css("border", "2px solid red");
-            $('#nameMessage').html("**First and last name compulsory");
-            $('#nameMessage').css("color", "red");
+            $(this).css("border", "2px solid #FF0000"),
+            $('#nameMessage').html("**Please Enter a valid name").css("color", "#FF0000");
 
         }
     });
 
     //age
-
     $("#age").keyup(function() {
         if (validateAge()) {
-            $('#age').css("border", "2px solid green");
-            $('#ageMessage').html("**Age Validated");
-            $('#ageMessage').css("color", "green");
+            $(this).css("border", "2px solid #008000"),
+            $('#ageMessage').html("**Age Validated").css("color", "#008000");
         } else {
-            $('#age').css("border", "2px solid red");
-            $('#ageMessage').html("**Please Enter a valid age");
-            $('#ageMessage').css("color", "red");
+            $(this).css("border", "2px solid #FF0000"),
+            $('#ageMessage').html("**Please Enter a valid age").css("color", "#FF0000");
         }
     });
 
     //phone 
     $('#phone').keyup(function() {
         if (validatePhone()) {
-            $('#phone').css("border", "2px solid green");
-            $('#phoneMessage').html("**Phone Number Validated");
-            $('#phoneMessage').css("color", "green");
+            $(this).css("border", "2px solid #008000"),
+            $('#phoneMessage').html("**Phone Number Validated").css("color", "#008000");
         } else {
-            $('#phone').css("border", "2px solid red");
-            $('#phoneMessage').html("**Please Enter a valid mobile number");
-            $('#phoneMessage').css("color", "red");
+            $(this).css("border", "2px solid #FF0000"),
+            $('#phoneMessage').html("**Please Enter a valid mobile number").css("color", "#FF0000");
         }
     });
 
     //enable and disable submit button
     $("#submit").prop('disabled', true);
-
     var toValidate = $('#name, #age, #email, #address'),
         valid = false;
 
@@ -122,54 +109,44 @@ $(document).ready(function() {
         });
     });
 
+    $('#resetting').click(function(){
+    	$('#formValid')[0].reset();
+    });
+
     //select country code and states based on country chosen
     $('#country').change(function() {
-        var val = $(this).val();
+    var city = $('#city');
+    var code = $('#code');
+    
+    var val = $(this).val();
         if (val == "India") {
-            $('#code').html("<option>+91</option>");
+            $(city).html("<option>Mumbai</option><option>Delhi</option><option>Indore</option");
+            $(code).html("<option>+91</option>");
         } else if (val == "America") {
-            $('#code').html("<option>+1</option>");
+            $(code).html("<option>+1</option>");
+            $(city).html("<option>New York</option><option>Los Angeles</option><option>Chicago</option>");
         } else if (val == "China") {
-            $('#code').html("<option>+86</option>");
+            $(code).html("<option>+86</option>");
+            $(city).html("<option>Beijing</option><option>Guangzhou</option><option>Taipei</option");
         } else if (val == "Japan") {
-            $('#code').html("<option>+81</option>");
+            $(code).html("<option>+81</option>");
+            $(city).html("<option> Tokyo</option><option>Guangzhou</option><option>Gold Coast</option");
         } else if (val == "Australia") {
-            $('#code').html("<option>+61</option>");
+            $(code).html("<option>+61</option>");
+            $(city).html("<option>Sydney</option><option>Melbourne</option><option>Osaka</option");
         } else if (val == "Sri Lanka") {
-            $('#code').html("<option>+94</option>");
+            $(code).html("<option>+94</option>");
+            $(city).html("<option>Colombo</option><option>Moratuwa</option><option>Jaffna</option");
         } else if (val == "item0") {
-            $("#code").html("<option value=''>--Country Code--</option>");
+            $(code).html("<option value=''>--Country Code--</option>");
+            $(city).html("<option value=''>--select one--</option>");
         }
-
     });
-
-    //select city based on country chosen
-    $('#country').change(function() {
-        var val = $(this).val();
-        if (val == "India") {
-            $('#city').html("<option>Mumbai</option><option>Delhi</option><option>Indore</option");
-        } else if (val == "America") {
-            $('#city').html("<option>New York</option><option>Los Angeles</option><option>Chicago</option>");
-        } else if (val == "China") {
-            $('#city').html("<option>Beijing</option><option>Guangzhou </option><option>Taipei </option");
-        } else if (val == "Japan") {
-            $('#city').html("<option> Tokyo</option><option>Guangzhou </option><option>Gold Coast</option");
-        } else if (val == "Australia") {
-            $('#city').html("<option>Sydney</option><option>Melbourne  </option><option>Osaka  </option");
-        } else if (val == "Sri Lanka") {
-            $('#city').html("<option>Colombo</option><option>Moratuwa  </option><option>Jaffna  </option");
-        } else if (val == "item0") {
-            $("#city").html("<option value=''>--select one--</option>");
-        }
-
-    });
-
 });
 
 function validateEmail() {
     //get value of input email
     var email = $('#email').val();
-
     //use regular expression
     var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
     if (reg.test(email)) {
@@ -182,7 +159,6 @@ function validateEmail() {
 function validateName() {
     //get value of input email
     var name = $('#name').val();
-
     //use regular expression
     var reg = /^[a-zA-Z]+ [a-zA-Z]+$/
     if (reg.test(name)) {
@@ -194,7 +170,6 @@ function validateName() {
 
 function validateAge() {
     var age = $('#age').val();
-
     if ((age >= 1) && (age <= 100)) {
         return true;
     } else {
