29a3ec7bf52fd78d7fa854e83e48f7d862eff05b
DeepRathore <rdeepika342@gmail.com>
2020-06-27 19:29:19 +0530

    update

--- Covid19_JqueryApi/assets/main.js
+++ Covid19_JqueryApi/assets/main.js
@@ -3,59 +3,53 @@ $(document).ready(function() {
         type: "GET",
         url: "https://api.covid19india.org/data.json",
         success: function(data) {
-
             //data in India Map part
             var totalStateWiseData = (data.statewise);
-            $("#tested").text(data.tested[103].totalsamplestested);
-            $("#date").text(totalStateWiseData[0].lastupdatedtime);
-            $("#confirmed").text(totalStateWiseData[0].confirmed),
-                $("#Deceased").text(totalStateWiseData[0].deaths),
-                $("#recovered").text(totalStateWiseData[0].recovered),
-                $("#active").text(totalStateWiseData[0].active);
+            $('#tested').text(data.tested[103].totalsamplestested);
+            $('#date').text(totalStateWiseData[0].lastupdatedtime);
+            $('#confirmed').text(totalStateWiseData[0].confirmed);
+            $('#deceased').text(totalStateWiseData[0].deaths);
+            $('#recovered').text(totalStateWiseData[0].recovered);
+            $('#active').text(totalStateWiseData[0].active);
 
             //search area   
-            var totallength = totalStateWiseData.length;
-            $("#button").click(function() {
-                var EnteredValue = $('.search').val();
-                for (var i = 0; i <= totallength; i++) {
-                    var result = (totalStateWiseData[i].state).replace(/"/g, "");
-                    if (result.toLowerCase() == EnteredValue.toLowerCase()) {
-                        let states = "<div>" + (totalStateWiseData[i].state).replace(/"/g, "") + "</div>";
-                        let confirmed = "<div>" + (totalStateWiseData[i].confirmed).replace(/"/g, "") + "</div>";
-                        let act = "<div>" + (totalStateWiseData[i].active).replace(/"/g, "") + "</div>";
-                        let recovered = "<div>" + (totalStateWiseData[i].recovered).replace(/"/g, "") + "</div>";
-                        let deseased = "<div>" +(totalStateWiseData[i].deaths).replace(/"/g, "") + "</div>";
-                        let update = "<div>" + (totalStateWiseData[i].lastupdatedtime).replace(/"/g, "") + "</div>";
-                        $('.NameState').html(states);
-                        $('.conState').html("Confirmed" + confirmed);
-                        $('.actState').html("Ativate" + act);
-                        $('.recState').html("Recovered" + recovered);
-                        $('.decState').html("Deseaced" + deseased);
-                        $('.update').html("Updated Last" + update);
+            var totalLength = totalStateWiseData.length;
+            $('#button').click(function() {
+                var enteredValue = $('.search').val();
+                for (var i = 0; i <= totalLength; i++) {
+                    var result = (totalStateWiseData[i].state);
+                    if (result.toLowerCase() == enteredValue.toLowerCase()) {
+                        let states = '<div>' + (totalStateWiseData[i].state) + '</div>';
+                        let confirmed = '<div>' + (totalStateWiseData[i].confirmed) + '</div>';
+                        let act = '<div>' + (totalStateWiseData[i].active) + '</div>';
+                        let recovered = '<div>' + (totalStateWiseData[i].recovered) + '</div>';
+                        let deseased = '<div>' + (totalStateWiseData[i].deaths) + '</div>';
+                        let update = '<div>' + (totalStateWiseData[i].lastupdatedtime) + '</div>';
+                        let note = '<div>' + (totalStateWiseData[i].statenotes) + '</div>';
+                        $('.nameState').html(states);
+                        $('.confirmState').html('Confirmed' + confirmed);
+                        $('.activeState').html('Ativate' + act);
+                        $('.recoveredState').html('Recovered' + recovered);
+                        $('.deceasedState').html('Deseaced' + deseased);
+                        $('.update').html('Updated Last' + update);
+                        $('.stateNote').html(note);
                     }
                 }
             });
 
             //table of state information
-            var text, con, act, rec, dec = '';
-            for (var i = 0; i <= totallength; i++) {
-                let states = "<tr>";
-                let confirmed = "<tr>";
-                let active = "<tr>";
-                let recovered = "<tr>";
-                let deseased = "<tr>";
-                states += "<td>" + (totalStateWiseData[i].state).replace(/"/g, "") + "</td>";
-                confirmed += "<td>" + (totalStateWiseData[i].confirmed).replace(/"/g, "") + "</td>";
-                active += "<td>" +(totalStateWiseData[i].active).replace(/"/g, "") + "</td>";
-                recovered += "<td>" +(totalStateWiseData[i].recovered).replace(/"/g, "") + "</td>";
-                deseased += "<td>" +(totalStateWiseData[i].deaths).replace(/"/g, "") + "</td>";
-                text += states;
-                con += confirmed;
-                act += active;
-                dec += deseased;
-                rec += recovered;
-                $('#Name').html(text), $('#con').html(con), $('#act').html(act),
-                    $('#rec').html(rec), $('#dec').html(dec);
+            var states, confirmed, active, recovered, deseased = '';
+            for (var i = 0; i <= totalLength; i++) {
+                states += '<tr><td>' + (totalStateWiseData[i].state) +  '</td></tr>';
+                confirmed += '<tr><td>' + (totalStateWiseData[i].confirmed) + '</td></tr>';
+                active += '<tr><td>' + (totalStateWiseData[i].active) +  '</td></tr>';
+                recovered += '<tr><td>'+ (totalStateWiseData[i].recovered) + '</td></tr>';
+                deseased += '<tr><td>' + (totalStateWiseData[i].deaths) +  '</td></tr>';
+                $('#name').html(states);
+                $('#con').html(confirmed);
+                $('#act').html(active);
+                $('#rec').html(recovered);
+                $('#dec').html(deseased);
             }
         }
     });
@@ -66,14 +60,15 @@ $(document).ready(function() {
         'Telangana', 'Jammu and Kashmir', 'Assam', 'Odisha', 'Punjab', 'Kerala', 'Uttarakhand', 'Chhattisgarh',
         'Jharkhand', 'Tripura', 'Ladakh', 'Goa', 'Himachal Pradesh', 'Manipur', 'Chandigarh', 'Puducherry',
         'Nagaland', 'Mizoram', 'Arunachal Pradesh', 'Sikkim', 'Dadra and Nagar Haveli and Daman and Diu',
-        'Andaman and Nicobar Islands', 'Meghalaya', 'Lakshadweep'];
+        'Andaman and Nicobar Islands', 'Meghalaya', 'Lakshadweep'
+    ];
 
-    $("#search").autocomplete({
+    $('#search').autocomplete({
         source: availableTags
     });
 
     //triggering Enter key while search
-    $("#search").keyup(function(e) {
+    $('#search').keyup(function(e) {
         if (e.which == 13) {
             $('#button').trigger('click');
         }
