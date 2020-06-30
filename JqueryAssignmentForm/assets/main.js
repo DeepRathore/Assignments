130f990a0184941203e336c257154909cd6b1924
DeepRathore <rdeepika342@gmail.com>
2020-06-30 16:24:58 +0530

    after review

--- JqueryAssignmentForm/assets/main.js
+++ JqueryAssignmentForm/assets/main.js
@@ -1,16 +1,18 @@
 //slider for height and salary
 $(document).ready(function() {
-    $('#slider-salary').slider({
+    var sliderSalary = $('#slider-salary');
+    var sliderHeight = $('#slider-height')
+    $(sliderSalary).slider({
         value: 15000,
         min: 0,
         max: 50000,
         step: 5000,
         slide: function(event, ui) {
-            $('#salary').val("Rs." + ui.value)
+            $('#salary').val('Rs.' + ui.value)
         }
     });
 
-    $("#salary").val("Rs." + $("#slider-salary").slider("value"));
+    $('#salary').val('Rs.' + $(sliderSalary).slider('value'));
 
     $('#slider-height').slider({
         value: 150,
@@ -18,43 +20,39 @@ $(document).ready(function() {
         max: 200,
         step: 2,
         slide: function(event, ui) {
-            $('#height').val(ui.value + "cm")
+            $('#height').val(ui.value + 'cm');
         }
     });
-    $("#height").val($("#slider-height").slider("value") + "cm");
+    $('#height').val($(sliderHeight).slider('value') + 'cm');
 
     //for validations
     //email
     $('#email').keyup(function() {
         if (validateEmail()) {
-            $(this).css("border", "2px solid #008000"),
-            $('#message').html("**Email Validated").css("color", "#008000");
+            $(this).css('border', '2px solid #008000'),
+                $('#message').html("**Email Validated").css("color", "#008000");
         } else {
             $(this).css("border", "2px solid #FF0000"),
-            $('#message').html("**Enter A valid Email address").css("color", "#FF0000");
+                $('#message').html("**Enter A valid Email address").css("color", "#FF0000");
         }
     });
 
     //name
     $('#name').keyup(function() {
         if (validateName()) {
-            $(this).css("border", "2px solid #008000"),
             $('#nameMessage').html("**Name Validated").css("color", "#008000");
 
         } else {
-            $(this).css("border", "2px solid #FF0000"),
             $('#nameMessage').html("**Please Enter a valid name").css("color", "#FF0000");
 
         }
     });
 
     //age
-    $("#age").keyup(function() {
+    $('#age').keyup(function() {
         if (validateAge()) {
-            $(this).css("border", "2px solid #008000"),
             $('#ageMessage').html("**Age Validated").css("color", "#008000");
         } else {
-            $(this).css("border", "2px solid #FF0000"),
             $('#ageMessage').html("**Please Enter a valid age").css("color", "#FF0000");
         }
     });
@@ -62,19 +60,16 @@ $(document).ready(function() {
     //phone 
     $('#phone').keyup(function() {
         if (validatePhone()) {
-            $(this).css("border", "2px solid #008000"),
             $('#phoneMessage').html("**Phone Number Validated").css("color", "#008000");
         } else {
-            $(this).css("border", "2px solid #FF0000"),
             $('#phoneMessage').html("**Please Enter a valid mobile number").css("color", "#FF0000");
         }
     });
-
+    var x, y;
     //enable and disable submit button
-    $("#submit").prop('disabled', true);
-    var toValidate = $('#name, #age, #email, #address'),
-        valid = false;
-
+    $('#submit').prop('disabled', true);
+    var toValidate = $('#email, #address'); //manytime key is pressed on selecte element+condition
+    var valid = false;
     toValidate.keyup(function() {
         if ($(this).val().length > 0) {
             $(this).data('valid', true);
@@ -83,42 +78,37 @@ $(document).ready(function() {
         }
 
         toValidate.each(function() {
-            if ($(this).data('valid') == true && validateAge() && validateName() &&
-                validateEmail() && validatePhone()) {
+            if ($(this).data('valid') == true && validateAge() && validateName() && validatePhone()) {
                 valid = true;
+
             } else {
                 valid = false;
             }
         });
 
         if (valid === true) {
-            $("#submit").prop('disabled', false);
+            $('#submit').prop('disabled', false);
         } else {
-            $("#submit").prop('disabled', true);
+            $('#submit').prop('disabled', true);
         }
     });
 
     //to show all filled values after click
-    $("#submit").click(function() {
-        $('#recieved').html("Inputs Received As Following :")
-        alert("click to see the values Entered :")
-        var x = $("form").serializeArray();
+    $('#submit').click(function() {
+        $('#recieved').html('Inputs Received As Following :')
+        var x = $('form').serializeArray();
         $.each(x, function(i, field) {
-            $("#allValues").append("<br>" + field.name + " : " +
+            $('#allValues').append("<br>" + field.name + " : " +
                 field.value + " ");
         });
     });
 
-    $('#resetting').click(function(){
-    	$('#formValid')[0].reset();
-    });
-
     //select country code and states based on country chosen
     $('#country').change(function() {
-    var city = $('#city');
-    var code = $('#code');
-    
-    var val = $(this).val();
+        var city = $('#city');
+        var code = $('#code');
+
+        var val = $(this).val();
         if (val == "India") {
             $(city).html("<option>Mumbai</option><option>Delhi</option><option>Indore</option");
             $(code).html("<option>+91</option>");
@@ -142,52 +132,55 @@ $(document).ready(function() {
             $(city).html("<option value=''>--select one--</option>");
         }
     });
+
+
+    function validateEmail() {
+        //get value of input email
+        var email = $('#email').val();
+        //use regular expression
+        var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        if (reg.test(email)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function validateName() {
+        //get value of input email
+        var name = $('#name').val();
+        //use regular expression
+        var reg = /^[a-zA-Z]+ [a-zA-Z]+$/
+        if (reg.test(name)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function validateAge() {
+        var age = $('#age').val();
+        if ((age >= 1) && (age <= 100)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function validatePhone() {
+        var phone = $('#phone').val();
+        var reg = /^\d{10}$/;
+        if (reg.test(phone)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 });
 
-function validateEmail() {
-    //get value of input email
-    var email = $('#email').val();
-    //use regular expression
-    var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-    if (reg.test(email)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function validateName() {
-    //get value of input email
-    var name = $('#name').val();
-    //use regular expression
-    var reg = /^[a-zA-Z]+ [a-zA-Z]+$/
-    if (reg.test(name)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function validateAge() {
-    var age = $('#age').val();
-    if ((age >= 1) && (age <= 100)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function validatePhone() {
-    var phone = $('#phone').val();
-    var reg = /^\d{10}$/;
-    if (reg.test(phone)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 //to set the image preview
-var loadImg = function(event) {
+function loadImg(event) {
     $('#frame').attr('src', URL.createObjectURL(event.target.files[0]));
 }
